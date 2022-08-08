e7299edb44d984679ff1b8b5f21ac72e2f95e65c
Margarita Krok <gretchen_k@mail.ru>
2022-08-08 16:05:53 +0300

    feat: css

---
+++ mesto-react/src/components/App/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="page__container">
+      <header className="header">
+        <img src="./images/logo.svg" alt="Логотип" className="header__logo" />
+      </header>
+
+      <section className="profile">
+        <div className="profile__avatar"></div>
+        <div className="profile__info">
+          <h1 className="profile__name">Жак-Ив Кусто</h1>
+          <button type="button" className="profile__edit-button" aria-label="edit"></button>
+          <p className="profile__text">Исследователь океана</p>
+        </div>
+        <button type="button" className="profile__add-button" aria-label="add"></button>
+      </section>
+
+      <section className="cards">
+        <ul className="cards__list">
+        </ul>
+      </section>
+
+      <footer className="footer">
+        <p className="footer__copyright">&copy; 2022 Mesto Russia</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
