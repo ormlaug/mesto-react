fd191664412db288ad029e0523347213e12353f0
Margarita Krok <gretchen_k@mail.ru>
2022-08-28 19:50:09 +0300

    исправлена ошибка с currentUser

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,12 +12,12 @@ function Main(props) {
       <section className="profile">
           <div className="profile__avatar"
             onClick={props.onEditAvatar}
-            style={{ backgroundImage: `url(${currentUser.avatar})` }}>
+            style={{ backgroundImage: `url(${currentUser?.avatar})` }}>
           </div>
           <div className="profile__info">
-            <h1 className="profile__name">{currentUser.name}</h1>
+            <h1 className="profile__name">{currentUser?.name}</h1>
             <button type="button" className="profile__edit-button" aria-label="edit" onClick={props.onEditProfile}></button>
-            <p className="profile__text">{currentUser.about}</p>
+            <p className="profile__text">{currentUser?.about}</p>
           </div>
           <button type="button" className="profile__add-button" aria-label="add" onClick={props.onAddPlace}></button>
         </section>
